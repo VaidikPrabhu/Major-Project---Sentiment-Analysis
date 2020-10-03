b43e75e4ae1bd6df2a9a4e6d63f7111c00754ee9
Vaidik S Prabhu <54259949+VaidikPrabhu@users.noreply.github.com>
2020-10-03 17:34:17 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+ 
+df = pd.read_csv('/content/drive/My Drive/sentiment_review.csv')
+x = df.iloc[:,0].values # Review column as input
+y = df.iloc[:,1].values # Sentiment column as output
+st.title("Sentiment Analysis On Review")
+st.subheader('TFIFD Vectorizer')
+st.write('This project is based on Naive Bayes Classifier')
+ 
+text_model = Pipeline([('tfidf',TfidfVectorizer()),('model',MultinomialNB())])
+text_model.fit(x,y)
+message = st.text_area("Enter Text","Type Here ..")
+op = text_model.predict([message])
+if st.button("Predict"):
+  st.title(op)
